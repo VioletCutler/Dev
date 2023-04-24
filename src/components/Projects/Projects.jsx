24451e187a00b8a6d06cbeb9d38416f88a997af4
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2023-04-24 13:38:40 -0400

    updated styling on haunted mansion project page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,10 @@ const Projects = () => {
     <section>
       <h2 id="project-header">Project Portfolio</h2>
 
-      <article className="project-container" id="student-group-generator-container">
+      <article
+        className="project-container"
+        id="student-group-generator-container"
+      >
         <h3>Student Group Generator</h3>
         <div className="project-description">
           <h4>Overview</h4>
@@ -28,59 +31,76 @@ const Projects = () => {
         </div>
       </article>
       <article className="project-container" id="haunted-mansion-container">
-        <h3>Haunted Mansion</h3>
-        <div className="haunted-mansion-div">
-          <div className="project-links">
-            <ul>
-              <li>
-                <a href="https://haunted.gq/">Try it out</a>
-              </li>
-              <li>
-                <a href="https://github.com/Team-Grape/CapstoneProject">
-                  Github
-                </a>
-              </li>
-            </ul>
+        <div id="haunted-mansion-header">
+          <h3>Haunted Mansion</h3>
+          <div id="haunted-mansion-project-links" className="project-links">
+            <a
+              className="haunted-mansion-project-link"
+              href="https://haunted.gq/"
+            >
+              Try it out
+            </a>
+            <a
+              className="haunted-mansion-project-link"
+              href="https://github.com/Team-Grape/CapstoneProject"
+            >
+              Github
+            </a>
           </div>
-          <div id="haunted-mansion-image-container"><img
-            id="haunted-mansion-image"
-            src="https://user-images.githubusercontent.com/96203831/161592220-83f84c1c-9618-402b-8c6f-94c4e2d01088.jpg"
-            alt="An old victorian mansion with a gloomy sky"
-          ></img></div>
-          
-          <h4>Overview</h4>
-          <p className="paragraph-text project-description">
-            Haunted Mansion was my capstone project during the Grace Hopper
-            Program. I collaborated with Qina Zeng, Chen Ying Xin, and Mai
-            Atwell to build this fully playable 2-D point and click adventure
-            game. Haunted Mansion is built using Kaboom JS as it's main library.
+        </div>
+        <iframe
+          id="haunted-mansion-video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/JvRc0QAXa0Q"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          allowFullScreen
+        ></iframe>
+        <h4>Overview</h4>
+        <div className="paragraph-text project-description">
+          <p>
+            <span style={{fontWeight:'bold'}}>Haunted Mansion</span> was my capstone project during the Grace Hopper
+            Program. I collaborated with <a href='https://linkedin.com/in/qina-zeng/'>Qina Zeng</a>, <a href='https://linkedin.com/in/chen-ying-xin'>Chen Ying Xin</a>, and <a href='https://github.com/mai-gh'>Mai
+            Atwell</a> to build this fully playable 2-D point and click adventure
+            game. Players can explore different rooms, interact with and pick up different items, and solve puzzles.    The game is built using Kaboom JS as it's main library.
             However, once we began to scale the game it became clear that Kaboom
-            was not sufficie and is deployed on Github pages
+            was not sufficient and is deployed on Github pages
           </p>
         </div>
         <div>
-          
-          <img
-            id="haunted-mansion-layout-image"
-            src="https://user-images.githubusercontent.com/96203831/161591984-e2110053-b8c2-4f67-b163-d3a77a76888b.png"
-            alt="A map of the different room connections inside the haunted mansion"
-          ></img>
-          <h4>Technical Challenges</h4><div className='tech-stack-logo-container'>
-          <img
-            className="tech-stack-logo"
-            src="https://raw.githubusercontent.com/replit/kaboom/HEAD/kaboom.png"
-            alt="Kaboom JS Logo"
-          ></img>
-          <img
-            className="tech-stack-logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
-            alt="Yellow square with a dark grey J S"
-          ></img>
-          <img
-            className="tech-stack-logo"
-            src="https://avatars.githubusercontent.com/u/57169982?s=200&v=4"
-            alt="White mermaid tail on red background"
-          ></img></div>
+        <h4>Features</h4>
+        <div className="paragraph-text project-description">
+          <p>A lot of time and attention was paid to give the game features that improve a users experience:</p>
+          <ul>
+          <li>Sound effects and pop up messages that give a user feedback on their in-game actions</li>
+          <p>A message log to help a player keep track of previous clues</p>
+            <li>An options menu to adjust the volume of both music and sound effects separately</li>
+            <li>Save functionality that allows a user to continue their game from any computer</li>
+            <li>A toolbar at the bottom of the screen with item icons for easy reference</li>
+          </ul>
+        </div>
+        </div>
+        <div>
+          <h4>Technical Challenges</h4>
+          <div className="tech-stack-logo-container">
+            <img
+              className="tech-stack-logo"
+              src="https://raw.githubusercontent.com/replit/kaboom/HEAD/kaboom.png"
+              alt="Kaboom JS Logo"
+            ></img>
+            <img
+              className="tech-stack-logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+              alt="Yellow square with a dark grey J S"
+            ></img>
+            <img
+              className="tech-stack-logo"
+              src="https://avatars.githubusercontent.com/u/57169982?s=200&v=4"
+              alt="White mermaid tail on red background"
+            ></img>
+          </div>
           <div className="project-description">
             <h5>Kaboom JS and Javascript</h5>
             <p className="paragraph-text">
@@ -100,21 +120,17 @@ const Projects = () => {
               puzzle based adventure game required us to pay special attention
               to where clues and items were located. We experimented with
               Mermaid.js as a way of charting potential game paths a player
-              might encounter .
+              might encounter.
             </p>
           </div>
         </div>
-
-        <iframe
-          id="haunted-mansion-video"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/JvRc0QAXa0Q"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-          allowFullScreen
-        ></iframe>
+        <div id="haunted-mansion-layout-image-container"></div>
+        <img
+          id="haunted-mansion-layout-image"
+          className="test"
+          src="https://user-images.githubusercontent.com/96203831/161591984-e2110053-b8c2-4f67-b163-d3a77a76888b.png"
+          alt="A map of the different room connections inside the haunted mansion"
+        ></img>
       </article>
     </section>
   );
