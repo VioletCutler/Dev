f7045696ba85d209e2613f9a4a463eddfd6bf788
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2023-04-26 14:50:30 -0400

    trying a 404.html for deployment to github pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
     return (
     <nav>
-        <h1 id='navbar-header'><Link to='/'>Violet Cutler</Link></h1>
+        <h1><Link id='navbar-header' to='/'>Violet Cutler</Link></h1>
         <div id='navlinks'>
    
         <Link to='/about'>About</Link>
