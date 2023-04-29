95a0f5c4b4927694cd1eb3aa87546319946a4eae
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2023-04-29 18:24:44 -0400

    styling for min-width and styling for mobile first

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,20 +11,26 @@ const About = () => {
             className="left-about-photo"
             src="https://i.imgur.com/rp8I9BQ.jpg"
           />
-         My name is <br/><span id='name-and-pronouns'>Violet (they/she)</span> and I'm a programmer, artist and organizer living in Philadelphia. I graduated from the Grace Hopper Program at Fullstack Academy in April, 2022. I build fullstack web applications using the PERN stack and 
+         <span className="no-wrap">My name is </span><br/><span id='name-and-pronouns'>Violet (they/she)</span> and I'm a programmer, artist and organizer living in Philadelphia. I graduated from the Grace Hopper Program at Fullstack Academy in April, 2022. I build fullstack web applications using the PERN stack and have taken a special interest in accessibility on the web. I am currently working through the W3C Introduction to Web Accessibility Course. 
         </p>
       </article>
       <h2 className="card-header">Coding Proficiencies</h2>
       <article className='card-container about-container'>
         <ul id="coding-proficiency-list">
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg"/>Javascript</li>
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"/>Node</li>
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://w7.pngwing.com/pngs/545/451/png-transparent-node-js-express-js-javascript-solution-stack-web-application-others-angle-text-rectangle-thumbnail.png"/>Express</li>
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"/>React</li>
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1985px-Postgresql_elephant.svg.png"/>PostgreSQL</li>
-          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"/>Git & Github</li>
+          <span className="no-wrap"><li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg"/>Javascript</li></span>
+          <span className="no-wrap">
+          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"/>Node</li></span>
+          <span className="no-wrap">
+          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://w7.pngwing.com/pngs/545/451/png-transparent-node-js-express-js-javascript-solution-stack-web-application-others-angle-text-rectangle-thumbnail.png"/>Express</li></span>
+          <span className="no-wrap">
+          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"/>React</li></span>
+          <span className="no-wrap">
+          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1985px-Postgresql_elephant.svg.png"/>PostgreSQL</li></span>
+          <span className="no-wrap">
+          <li className="coding-proficiency-list-item"><img className="coding-proficiency-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"/>Git & Github</li></span>
         </ul>
       </article>
+      <h2 className="card-header">Coding projects</h2>
       <h2 className='card-header'>Life Before Coding</h2>
       <article className="card-container about-container">
       
